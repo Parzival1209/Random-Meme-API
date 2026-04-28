245519533d845cf6014a3e7d8479879a9b710cdf
Parzival1209 <kaikaiyaron@gmail.com>
2026-04-28 11:45:00 +0800

    Updated get-meme-metadata.js

--- get-meme-metadata.js
+++ get-meme-metadata.js
@@ -3,34 +3,6 @@ import sizeOf from "image-size";
 import fetch from "node-fetch";
 
 let memes = JSON.parse(fs.readFileSync("memes.json"));
-memes = memes.map(memeUrl => {
-
-  // Reddit media link handling
-  if (memeUrl.startsWith("https://www.reddit.com/media?url=")) {
-    const queryString = memeUrl.split("?")[1];
-    const params = {};
-
-    queryString.split("&").forEach(pair => {
-      const [key, value] = pair.split("=");
-      params[key] = value;
-    });
-
-    if (params.url) {
-      memeUrl = decodeURIComponent(params.url);
-    }
-  }
-
-  // Prepend missing protocols
-  if (!memeUrl.startsWith("http://") && !memeUrl.startsWith("https://") && !memeUrl.startsWith("Count")) {
-    if (memeUrl.startsWith("memes/")) {
-      memeUrl = "https://raw.githubusercontent.com/Parzival1209/random-meme-api/refs/heads/main/" + memeUrl;
-    } else {
-      memeUrl = "https://" + memeUrl;
-    }
-  }
-
-  return memeUrl;
-});
 
 async function getSize(url) {
   const arrayBuffer = await fetch(url).then(r => r.arrayBuffer());
